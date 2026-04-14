d11510232ff4a9c53dfc823afc6583c974e2cdd9
ChangYu Huang <jack.mainwork@gmail.com>
2026-04-13 20:05:30 -0400

    tab

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -3,3 +3,5 @@ local keymap = vim.keymap
 -- General
 keymap.set("", "<Space>", "<Nop>", { silent = true })
 keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "退回 Normal 模試" })
+keymap.set("v", "<Tab>", ">gv", { desc = "增加縮排" })
+keymap.set("v", "<S-Tab>", "<gv", { desc = "減少縮排" })
